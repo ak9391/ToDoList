62ed5017c184ed5219f155d6bfb1c64bc01cd02c
ak9391 <ak9391@nyu.edu>
2023-07-18 22:46:07 +0530

    Iteration 1 complete

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var toDoItems: [ToDoItem] = []
+    
     @State private var showNewTask = false
     
     var body: some View {
@@ -29,9 +31,20 @@ struct ContentView: View {
         }
         .padding()
         
+        List {
+                ForEach (toDoItems) { toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
+               }
+        }
+        .listStyle(.plain)
+
         if showNewTask {
-            NewToDoView(title: "", isImportant: false)
-                }
+            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+        }
     }
 }
 
